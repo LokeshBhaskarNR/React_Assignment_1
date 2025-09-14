d5220208ca5466804845f12e97db77b22ac46bd5
Lokesh Bhaskar <139795809+LokeshBhaskarNR@users.noreply.github.com>
2025-09-14 16:15:48 +0530

    push first assignment

---
+++ Environment_Setup_And_First_React_App/about_me_page/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Name is Lokesh Bhaskar</h1>
+      <h2>I am learning React JS</h2>
+      <img src={logo} className="App-logo" alt="logo" />
+
+    </div>
+
+  );
+}
+
+export default App;
